69d3c388d0b007bdb7f2e5600dfe30589fd568dc
Danial Kh <khoshkholgh.danial@gmail.com>
2018-01-09 20:17:50 +0330

    Initial commit Basic Analyze (Score - Possession - Average Stamina) More work in progress

---
+++ Analyzer.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import numpy as np
+import xml.etree.ElementTree as et
+import math
+
+class Analyzer:
+    def __init__(self):
+        self.rcgPath = None
+        self.cycles = None
+        self.teams = {}
+
+    def set_rcg_file(self, path):
+        self.rcgPath = path
+
+    def dist(self,x1,y1,x2,y2):
+        return math.sqrt((x1-x2)**2+(y1-y2)**2)
+
+    def find_player_in_possess(self, cycle):
+        if cycle == 3000:
+            return 0
+        current_cycle = self.cycles.iloc[cycle - 1].copy()
+        dist_left = np.array([])
+        dist_right = np.array([])
+        for player in current_cycle['Left']:
+            tmp = np.array([self.dist(float(player['PosX']), float(player['PosY']), float(current_cycle['Ball']['PosX']),
+                                     float(current_cycle['Ball']['PosY']))])
+
+            dist_left = np.concatenate((dist_left, tmp))
+        for player in current_cycle['Right']:
+            tmp = np.array([self.dist(float(player['PosX']), float(player['PosY']), float(current_cycle['Ball']['PosX']),
+                                     float(current_cycle['Ball']['PosY']))])
+            dist_right = np.concatenate((dist_right, tmp))
+
+        min_left = np.amin(dist_left)
+        min_right = np.amin(dist_right)
+
+
+        if math.fabs(min_left-min_right)<0.3:
+            # print('Cycle {} no one is in possession'.format(cycle))
+            return 0
+        elif min_left < min_right and min_left < 6 :
+            # print('Cycle {} closest player is Left {} with distance = {}'.format(cycle, np.argmin(dist_left)+1, min_left))
+            return np.argmin(dist_left)+1
+        elif min_left > min_right and min_right < 6:
+            # print('Cycle {} closest player is Right {} with distance = {}'.format(cycle, np.argmin(dist_right)+12, min_right))
+            return np.argmin(dist_right)+12
+        else:
+            # print('Cycle {} no one is in possession'.format(cycle))
+            return 0
+
+    def extract_rcg_file(self):
+        self.cycles = pd.DataFrame(columns=["Left", "Right", "Ball"], index=np.arange(6000))
+        tree = et.parse(self.rcgPath)
+        root = tree.getroot()
+        for node in root:
+            if node.tag == 'Team':
+                if node.attrib['Side'] == 'Left':
+                    self.teams['Left'] = {'Name':node.attrib['Name'], 'Score':int(node.attrib['Goals'])}
+                else:
+                    self.teams['Right'] = {'Name':node.attrib['Name'],'Score':int(node.attrib['Goals'])}
+
+            elif node.tag == 'Cycle':
+                self.cycles.iloc[int(node.attrib['Number']) - 1]['Left'] = []
+                self.cycles.iloc[int(node.attrib['Number']) - 1]['Right'] = []
+                current_cycle = self.cycles.iloc[int(node.attrib['Number']) - 1]
+                for movable in node:
+                    if movable.tag == 'Ball':
+                        current_cycle['Ball'] = movable.attrib
+                    elif movable.tag == 'Player':
+                        if movable.attrib['Side'] == 'Left':
+                            current_cycle['Left'].append(movable.attrib)
+                        else:
+                            current_cycle['Right'].append(movable.attrib)
+
+    def analyze_possession(self):
+        left_possess_count = 0
+        right_possess_count = 0
+
+        for i in range(6000):
+            closest_player = self.find_player_in_possess(i + 1)
+            if closest_player < 12 and closest_player != 0:
+                left_possess_count += 1
+            elif closest_player > 11 and closest_player != 0:
+                right_possess_count += 1
+
+        print('Left Possession is {} and Right Possession is {}'.format(left_possess_count, right_possess_count))
+
+        return left_possess_count, right_possess_count
+
+    def analyze_stamina(self):
+        totalLeft = 0
+        totalRight = 0
+
+        for idx, row in self.cycles.iterrows():
+            if type(row['Left']) == list:
+                for player in row['Left']:
+                    totalLeft += float(player['Stamina'])
+                for player in row['Right']:
+                    totalRight += float(player['Stamina'])
+
+
+        print('Left Stamina is {} and Right Stamina is {}'.format(totalLeft,totalRight))
+
+        return totalLeft/65989, totalRight/65989
+
+if __name__ == '__main__':
+    analyzer = Analyzer()
+    analyzer.set_rcg_file('C:/Users/MSI/Documents/Python Scripts/RcssAnalyzer/gamefile.xml')
+    analyzer.extract_rcg_file()
---
+++ main.py
@@ -0,0 +1,205 @@
+import tkinter as tk
+import Analyzer as alz
+from tkinter import  Menu
+from tkinter import ttk
+from tkinter import filedialog
+####################
+#### Functions
+####################
+def _quit():
+    win.quit()
+    win.destroy()
+    exit()
+
+def _open_log_file():
+    analyzer = alz.Analyzer()
+    analyzer.set_rcg_file(filedialog.askopenfilename())
+    analyzer.extract_rcg_file()
+    tmpLeft, tmpRight = analyzer.analyze_possession()
+    total = tmpLeft + tmpRight
+    global possessionLeft, possessionRight
+    possessionLeft.set(str(100 * tmpLeft / total)[:5]+' %')
+    possessionRight.set(str(100 * tmpRight/total)[:5]+' %')
+
+    global teamNameLeft, teamNameRight
+    teamNameLeft.set(analyzer.teams['Left']['Name'])
+    teamNameRight.set(analyzer.teams['Right']['Name'])
+
+    global goalsLeft, goalsRight
+    goalsLeft.set(str(analyzer.teams['Left']['Score']))
+    goalsRight.set(str(analyzer.teams['Right']['Score']))
+
+    global staminaLeft, staminaRight
+    tmpLeft,tmpRight = analyzer.analyze_stamina()
+    staminaLeft.set(str(tmpLeft))
+    staminaRight.set(str(tmpRight))
+
+
+
+###################
+###### Procedural Code
+###################
+
+## Create Window
+win = tk.Tk()
+
+win.title("RCSS Analyzer")
+win.minsize(height=300, width=360)
+win.resizable(0, 0)
+## Add Menu
+
+menuBar = Menu()
+win.config(menu=menuBar)
+
+## Add File Menu
+
+fileMenu = Menu(menuBar, tearoff=0)
+fileMenu.add_command(label='Open Log File',command=_open_log_file)
+fileMenu.add_separator()
+fileMenu.add_command(label='Open Results File')
+fileMenu.add_command(label='Save Results')
+fileMenu.add_separator()
+fileMenu.add_command(label='Exit', command=_quit)
+menuBar.add_cascade(label='File', menu=fileMenu)
+
+## Add Help Menu
+helpMenu = Menu(menuBar, tearoff=0)
+helpMenu.add_command(label='About')
+menuBar.add_cascade(label='Help', menu=helpMenu)
+
+## Tab Control
+tabControl = ttk.Notebook(win)
+
+statsTab = ttk.Frame(tabControl)
+tabControl.add(statsTab, text='Stats')
+
+graphTab = ttk.Frame(tabControl)
+tabControl.add(graphTab, text='Graph')
+
+tabControl.pack(expand=1, fill='both')
+
+## Creating Frame & Setting up grid
+main_frame = ttk.LabelFrame(statsTab, text='Statistics')
+
+main_frame.grid(column=0, row=0, padx=8, pady=4)
+
+#### Adding Labels
+
+ttk.Label(main_frame, text='Left').grid(column=1, row=0)
+ttk.Label(main_frame, text='Right').grid(column=2, row=0)
+
+MAX_LABEL_WIDTH = 20
+
+ttk.Label(main_frame, text='Team Name',width=MAX_LABEL_WIDTH).grid(column=0, row=1)
+ttk.Label(main_frame, text='Possession',width=MAX_LABEL_WIDTH).grid(column=0, row=2)
+ttk.Label(main_frame, text='Goals',width=MAX_LABEL_WIDTH).grid(column=0, row=3)
+ttk.Label(main_frame, text='Shots',width=MAX_LABEL_WIDTH).grid(column=0, row=4)
+ttk.Label(main_frame, text='Shot Accuracy',width=MAX_LABEL_WIDTH).grid(column=0, row=5)
+ttk.Label(main_frame, text='Passes',width=MAX_LABEL_WIDTH).grid(column=0, row=6)
+ttk.Label(main_frame, text='Pass Accuracy',width=MAX_LABEL_WIDTH).grid(column=0, row=7)
+ttk.Label(main_frame, text='Opportunities',width=MAX_LABEL_WIDTH).grid(column=0, row=8)
+ttk.Label(main_frame, text='Saves',width=MAX_LABEL_WIDTH).grid(column=0, row=9)
+ttk.Label(main_frame, text='Clearances',width=MAX_LABEL_WIDTH).grid(column=0, row=10)
+ttk.Label(main_frame, text='Avg. Stamina',width=MAX_LABEL_WIDTH).grid(column=0, row=11)
+
+#### Text Entries
+
+MAX_ENTRY_WIDTH = 12
+ENTRY_IPADY = 5
+ENTRY_PADY = 3
+ENTRY_PADX = 5
+
+teamNameLeft = tk.StringVar()
+teamNameLeftEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=teamNameLeft, state='readonly')
+teamNameLeftEntry.grid(column=1, row=1,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+teamNameRight = tk.StringVar()
+teamNameRightEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=teamNameRight, state='readonly')
+teamNameRightEntry.grid(column=2, row=1,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+possessionLeft = tk.StringVar()
+possessionLeftEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=possessionLeft, state='readonly')
+possessionLeftEntry.grid(column=1, row=2,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+possessionRight = tk.StringVar()
+possessionRightEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=possessionRight, state='readonly')
+possessionRightEntry.grid(column=2, row=2,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+goalsLeft = tk.StringVar()
+goalsLeftEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=goalsLeft, state='readonly')
+goalsLeftEntry.grid(column=1, row=3,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+goalsRight = tk.StringVar()
+goalsRightEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=goalsRight, state='readonly')
+goalsRightEntry.grid(column=2, row=3,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+shotsLeft = tk.StringVar()
+shotsLeftEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=shotsLeft, state='readonly')
+shotsLeftEntry.grid(column=1, row=4,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+shotsRight = tk.StringVar()
+shotsRightEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=shotsRight, state='readonly')
+shotsRightEntry.grid(column=2, row=4,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+shotAccLeft = tk.StringVar()
+shotAccLeftEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=shotAccLeft, state='readonly')
+shotAccLeftEntry.grid(column=1, row=5,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+shotAccRight = tk.StringVar()
+shotAccRightEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=shotAccRight, state='readonly')
+shotAccRightEntry.grid(column=2, row=5,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+passesLeft = tk.StringVar()
+passesLeftEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=passesLeft, state='readonly')
+passesLeftEntry.grid(column=1, row=6,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+passesRight = tk.StringVar()
+passesRightEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=passesRight, state='readonly')
+passesRightEntry.grid(column=2, row=6,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+passAccLeft = tk.StringVar()
+passAccLeftEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=passAccLeft, state='readonly')
+passAccLeftEntry.grid(column=1, row=7,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+passAccRight = tk.StringVar()
+passAccRightEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=passAccRight, state='readonly')
+passAccRightEntry.grid(column=2, row=7,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+opportunitiesLeft = tk.StringVar()
+opportunitiesLeftEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=opportunitiesLeft, state='readonly')
+opportunitiesLeftEntry.grid(column=1, row=8,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+opportunitiesRight = tk.StringVar()
+opportunitiesRightEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=opportunitiesRight, state='readonly')
+opportunitiesRightEntry.grid(column=2, row=8,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+savesLeft = tk.StringVar()
+savesLeftEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=savesLeft, state='readonly')
+savesLeftEntry.grid(column=1, row=9,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+savesRight = tk.StringVar()
+savesRightEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=savesRight, state='readonly')
+savesRightEntry.grid(column=2, row=9,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+clearancesLeft = tk.StringVar()
+clearancesLeftEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=clearancesLeft, state='readonly')
+clearancesLeftEntry.grid(column=1, row=10,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+clearancesRight = tk.StringVar()
+clearancesRightEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=clearancesRight, state='readonly')
+clearancesRightEntry.grid(column=2, row=10,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+staminaLeft = tk.StringVar()
+staminaLeftEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=staminaLeft, state='readonly')
+staminaLeftEntry.grid(column=1, row=11,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+staminaRight = tk.StringVar()
+staminaRightEntry = ttk.Entry(main_frame, width=MAX_ENTRY_WIDTH, textvariable=staminaRight, state='readonly')
+staminaRightEntry.grid(column=2, row=11,ipady=ENTRY_IPADY, pady=ENTRY_PADY, padx=ENTRY_PADX)
+
+
+
+
+## Start GUI
+
+win.mainloop()
